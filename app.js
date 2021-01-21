2971b48c8bc5868d34dda56ce29819b6a428531f
RRKarthik <raviramkarthik7@gmail.com>
2021-01-21 11:05:56 +0530

    refactored code and added changes to display the result in a proper manner

--- app.js
+++ app.js
@@ -13,19 +13,15 @@ function play(playerMove,computerMove){
     if(playerMove == computerMove){
         return;
     }else if(playerMove == "rock" && computerMove == "paper" || playerMove == "scissors" && computerMove == "rock" || playerMove == "paper" && computerMove == "scissors"){
-       if(playerMove=="rock")computerScore++;
-       if(playerMove=="paper")computerScore++;
-       if(playerMove=="scissors")computerScore++;
+       computerScore++;
 
     }
     else{
-        if(playerMove=="rock")playerScore++;
-       if(playerMove=="paper")playerScore++;
-       if(playerMove=="scissors")playerScore++;
+        playerScore++;
     }
 }
 
-function setPlayerMove(){
+function setMove(){
    playerMove = this.getAttribute("data-choice");
    computerMove = computerPlay();
    const cpuSelect = document.querySelector(`[data-cpu=${computerMove}]`);
@@ -36,30 +32,27 @@ function setPlayerMove(){
  
    play(playerMove,computerMove);
    updateScore();
-       if(playerScore == 5){
-           const playerDiv = document.querySelector("#player-div");
-           console.log(playerDiv);
-           setInterval(function(){
-               playerDiv.classList.toggle("winner");
-               setTimeout(function(){playerDiv.classList.toggle("winner");},1000);
-           },2000);
-           if(!alert("You Won!")){
-            location.reload();
-        }
-       }
-       if(computerScore == 5){
-           const cpuDiv = document.querySelector("#cpu-div");
-           console.log(cpuDiv);
-           setInterval(function(){
-            cpuDiv.classList.toggle("winner");
-            setTimeout(function(){cpuDiv.classList.toggle("winner");},500);
-        },2000);
-        if(!alert("You Lost!")){
-            location.reload();
-        }
-       
-       }
+   checkScore(); 
    
+}
+
+function checkScore(){
+    if(playerScore == 5){
+        const announcer = document.querySelector("#announcer-para");
+        console.log(announcer);
+        announcer.textContent = "You Won!";
+        const container = document.getElementById("container");
+   container.style.cssText = "display:none;"
+    }
+    if(computerScore == 5){
+        const announcer = document.querySelector("#announcer-para");
+        console.log(announcer);
+        announcer.textContent = "You Lost!";
+        const container = document.getElementById("container");
+   container.style.cssText = "display:none;"
+    }
+   
+
 }
 
 
@@ -73,7 +66,7 @@ function updateScore(){
 score.textContent = `${playerScore} - ${computerScore}`;
 
 const buttons = document.querySelectorAll(".player");
-buttons.forEach(button => button.addEventListener("click",setPlayerMove) );
+buttons.forEach(button => button.addEventListener("click",setMove) );
 
 
 
